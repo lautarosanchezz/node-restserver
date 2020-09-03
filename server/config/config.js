4fce041c360619d29bcd751d4dcb476b6645a0dc
Lautaro <lautarodimcheff.developer@outlook.com>
2020-09-02 21:04:08 -0300

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,6 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://lauta:LflMZfK0pX57Vunf@cluster0.jhcgu.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
-
 process.env.URLDB = urlDB;
